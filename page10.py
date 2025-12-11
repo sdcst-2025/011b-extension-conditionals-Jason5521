5044d8e50ef7146ac4abae192514f29a77885319
jason5521 <jasonb185@deltalearns.ca>
2025-12-11 08:47:43 -0800

    saved changes

--- page10.py
+++ page10.py
@@ -3,8 +3,10 @@ from os import system
 system("clear||cls")
 print("There is no safe option for that input.")
 print("")
-print("The beast of nightmares caught up to you, and tore you apart.")
+print("The creature caught up to you, and tore you apart.")
 print("")
 print("No one will come looking for you. your soul is forever lost in the dark corners of the forest.")
 print("")
 print("The end!")
+
+print("go back to page 1 to try again.")
--- page8.py
+++ page8.py
@@ -2,7 +2,7 @@ from rich import print
 from os import system
 system("clear||cls")
 
-print("[red on white]You decide to break into the lighthouse.")
+print("You decide to break into the lighthouse.")
 print("")
 print("Inspecting the door for weaknesses, you find nothing.")
 print("")
@@ -16,7 +16,7 @@ print("The front door unlocks and opens, from one button in the wall.")
 print("")
 print("You cover up the hole with some dirt and head inside.")
 print("")
-print("Within the lighthouse lies a radio, a device of unknown origin, and the stairs to the top.")
+print("Within the lighthouse lies a radio, a device labeled [black on yellow]*DO NOT TOUCH*[/black on yellow], and the stairs to the top.")
 print("")
 choice = input("To try and use the radio, enter a 9. to go up the stairs, enter a 13.")
 if choice == "9":
